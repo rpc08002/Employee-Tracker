f4027655b4705fbf412c1eda7a5781b98fad311c
rpc08002 <rcurtin0491@gmail.com>
2021-02-02 11:16:00 -0500

    added department function

--- app.js
+++ app.js
@@ -134,3 +134,60 @@ var connection = mysql.createConnection({
       }
     })
   };
+
+  addDepartment = () => {
+    inquirer.prompt([
+      {
+        name: "department",
+        type: "input",
+        message: "What department would you like to add?"
+      }
+    ]).then(function(answer) {
+      connection.query(`INSERT INTO department (name) VALUES ('${answer.department}')`, (err, res) => {
+        if (err) throw err;
+        console.log("1 new department added: " + answer.department);
+        getDepartments();
+        start();
+      }) 
+    })
+  };
+  
+  addRole = () => {
+    let departmentOptions = [];
+    for (i = 0; i < departments.length; i++) {
+      departmentOptions.push(Object(departments[i]));
+    };
+  
+    inquirer.prompt([
+      {
+        name: "title",
+        type: "input",
+        message: "What role would you like to add?"
+      },
+      {
+        name: "salary",
+        type: "input",
+        message: "What is the salary for this possition?"
+      },
+      {
+        name: "department_id",
+        type: "list",
+        message: "What is the department for this possition?",
+        choices: departmentOptions
+      },
+    ]).then(function(answer) {
+      for (i = 0; i < departmentOptions.length; i++) {
+        if (departmentOptions[i].name === answer.department_id) {
+          department_id = departmentOptions[i].id
+        }
+      }
+      connection.query(`INSERT INTO role (title, salary, department_id) VALUES ('${answer.title}', '${answer.salary}', ${department_id})`, (err, res) => {
+        if (err) throw err;
+  
+        console.log("1 new role added: " + answer.title);
+        getRoles();
+        start();
+      }) 
+    })
+  };
+  
