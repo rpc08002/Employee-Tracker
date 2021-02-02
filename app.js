f63dd815d8b56645f38212bbe4c3279501686e5a
rpc08002 <rcurtin0491@gmail.com>
2021-02-02 11:09:39 -0500

    declared variables, set upp connection to mysql

---
+++ app.js
@@ -0,0 +1,29 @@
+
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const { printTable } = require('console-table-printer');
+const figlet = require('figlet');
+let roles;
+let departments;
+let managers;
+let employees;
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "password",
+    database: "employees_db"
+  });
+
+  figlet('FSC Employee Tracker', (err, result) => {
+    console.log(err || result);
+  });
+
+  
